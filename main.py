df848931e156f0f6a2353a38337dcd23a8564f1c
plcking777 <woutvandamme.wvd@gmail.com>
2025-11-07 20:09:05 +0100

    restore image structure as input

--- main.py
+++ main.py
@@ -7,22 +7,24 @@ import pandas as pd
 df = pd.read_csv("data/mnist-small.csv")
 
 
-train_data = df[0:]
+train_data = np.array(df).T[1:].T  # pixels (removed the labels)
 train_labels = df.label
 
+
+
+train_data = np.array([np.reshape(entry, (28, 28)) for entry in train_data])  # restore structure of the image
 
 
 
 print("shape:  ", train_data.shape)
 
 model = Model([
-    Convolution(1, (3, 3), 1, (28, 28)), # 28x28 -> convolution size 26x26 (x kernel_count)
+    Convolution(1, (3, 3), 1, (28, 28)), # data_countx28x28 -> convolution size 26x26 (x kernel_count)
     Flatten(), # 26x26 -> 676
     Dense(676, None),
     Dense(256, relu),
     Dense(10, softmax),
 ])
-
 
 """
 model.set_input(train_data)
